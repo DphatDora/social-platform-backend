b5275c4fb82216fc2a83edeb04eb64e65126aa1c
DphatDora <hdphat123@gmail.com>
2025-11-21 12:11:26 +0700

    add isFollow in commmunity response

--- src/internal/interface/dto/response/community_response.go
+++ src/internal/interface/dto/response/community_response.go
@@ -39,6 +39,7 @@ func NewCommunityDetailResponse(community *model.Community) *CommunityDetailResp
 		CreatedAt:                community.CreatedAt,
 		IsRequiresMemberApproval: community.RequiresMemberApproval,
 		IsRequiresPostApproval:   community.RequiresPostApproval,
+		IsFollow:                 community.IsSubscribed,
 	}
 }
 
